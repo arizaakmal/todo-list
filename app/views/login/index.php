4320c2e670e139b846ef5dc93d8c8650f8668222
fajarizkiy7 <fjr.rzk19@gmail.com>
2023-06-26 19:22:15 +0700

    update tampilan login

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -9,9 +9,9 @@
     </style>
 </head>
 
-<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-  <div class="col-lg-6">
-    <h2 class="text-center">Login</h2>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh; ">
+  <div class="col-lg-3 rounded p-4 bg-secondary">
+    <h2 class="text-center">LOGIN</h2>
     <div class="row">
       <div class="col-lg-12">
         <?php Flasher::flash(); ?>
@@ -27,9 +27,8 @@
         <label for="password">Password:</label>
         <input type="password" class="form-control" name="password" id="password" required>
       </div>
-
-      <button type="submit" class="btn btn-primary">Login</button>
+      <button type="submit" class="btn btn-primary col-sm-12 mt-4">Login</button>
     </form>
-    <p class="text-center">Don't have an account? <a href="<?= BASEURL; ?>/register">Register here</a></p>
+    <p class="text-center mt-2">Don't have an account? <a href="<?= BASEURL; ?>/register">Register here</a></p>
   </div>
 </div>
